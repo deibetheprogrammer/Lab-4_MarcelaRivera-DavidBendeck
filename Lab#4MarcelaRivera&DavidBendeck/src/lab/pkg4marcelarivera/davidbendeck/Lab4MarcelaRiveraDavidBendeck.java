e7ce4b00cd6aa34b427543abd9ce3621630d4988
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-02-14 16:29:58 -0600

    Cambios al main

--- Lab#4MarcelaRivera&DavidBendeck/src/lab/pkg4marcelarivera/davidbendeck/Lab4MarcelaRiveraDavidBendeck.java
+++ Lab#4MarcelaRivera&DavidBendeck/src/lab/pkg4marcelarivera/davidbendeck/Lab4MarcelaRiveraDavidBendeck.java
@@ -22,9 +22,56 @@ public class Lab4MarcelaRiveraDavidBendeck {
             int op = leer.nextInt();
             switch (op) {
                 case 1:
+                    System.out.print("CRUD equipo:\n"
+                            + "1) Agregar equipo\n"
+                            + "2) Modificar equipo\n"
+                            + "3) Eliminar equipo\n"
+                            + "4) Listar equipos\n"
+                            + "Su elección:");
+                    op = leer.nextInt();
+                    vs = leer.nextLine();
                     
+                    switch(op) {
+                        case 1:
+                            System.out.println("Ingrese la casa (Gryffindor, Slytherin, Ravenclaw, Hufflepuff)");
+                            String casa = leer.nextLine();
+                            
+                            
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                    }
                     break;
                 case 2:
+                    System.out.print("CRUD jugador:\n"
+                            + "1) Agregar jugador\n"
+                            + "2) Modificar jugador\n"
+                            + "3) Eliminar jugador\n"
+                            + "4) Listar jugador\n"
+                            + "Su elección:");
+                    op = leer.nextInt();
+                    vs = leer.nextLine();
+                    
+                    switch(op) {
+                        case 1:
+                            System.out.println("Ingrese la casa (Gryffindor, Slytherin, Ravenclaw, Hufflepuff)");
+                            String casa = leer.nextLine();
+                            
+                            System.out.println("Ingrese el nombre: ");
+                            String nombre = leer.nextLine();
+                            
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                    }
       
                     break;
                 case 3:
