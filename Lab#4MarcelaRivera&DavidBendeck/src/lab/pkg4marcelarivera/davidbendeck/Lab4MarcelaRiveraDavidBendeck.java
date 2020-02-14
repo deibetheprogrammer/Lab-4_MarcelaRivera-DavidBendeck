aa4f32d5b0a3f5aadfda674a7c92434018dfe73e
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-02-14 16:42:16 -0600

    no se

--- Lab#4MarcelaRivera&DavidBendeck/src/lab/pkg4marcelarivera/davidbendeck/Lab4MarcelaRiveraDavidBendeck.java
+++ Lab#4MarcelaRivera&DavidBendeck/src/lab/pkg4marcelarivera/davidbendeck/Lab4MarcelaRiveraDavidBendeck.java
@@ -64,6 +64,8 @@ public class Lab4MarcelaRiveraDavidBendeck {
                             System.out.println("Ingrese el nombre: ");
                             String nombre = leer.nextLine();
                             
+                            
+                            
                             break;
                         case 2:
                             break;
