5e5edc2578e57b25261fff8741fc265387c22552
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-02-14 16:26:52 -0600

    Modificaciones al main

--- Lab#4MarcelaRivera&DavidBendeck/src/lab/pkg4marcelarivera/davidbendeck/Lab4MarcelaRiveraDavidBendeck.java
+++ Lab#4MarcelaRivera&DavidBendeck/src/lab/pkg4marcelarivera/davidbendeck/Lab4MarcelaRiveraDavidBendeck.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class Lab4MarcelaRiveraDavidBendeck {
 
     static Scanner leer = new Scanner(System.in);
+    static String vs;
 
     public static void main(String[] args) {
         while (true) {
@@ -21,9 +22,10 @@ public class Lab4MarcelaRiveraDavidBendeck {
             int op = leer.nextInt();
             switch (op) {
                 case 1:
+                    
                     break;
                 case 2:
-                    System.out.println("1.Agregar CRUD\n2.Modificar CRUD\n3.Eliminar CRUD\nListar\nVer STATS de un jugador");
+      
                     break;
                 case 3:
                     break;
@@ -32,58 +34,5 @@ public class Lab4MarcelaRiveraDavidBendeck {
                     break;
             }
         }
-    }
-    static String casa = "";
-    static int partidos_ganados,partidos_perdidos;
-
-    public static void llenar() {
-        System.out.println("1.Gryffindor\n2.Slytherin\n3.Ravenclaw\n4.Hufflepuff");
-        int op = leer.nextInt();
-        switch (op) {
-            case 1:
-                casa = "Gryffindor";
-                break;
-            case 2:
-                casa = "Slytherin";
-                break;
-            case 3:
-                casa = "Ravenclaw";
-                break;
-            case 4:
-                casa = "Hufflepuff";
-                break;
-
-        }
-        System.out.println("Cantidad de partidos ganados: ");
-        partidos_ganados=leer.nextInt();
-        System.out.println("Cantidad dde partidos perdidos: ");
-        partidos_perdidos=leer.nextInt();
-        
-    }
-    static String nombre,casa_jugador;
-    static int año,n_uniforme;
-    public static void jugadores(){
-        System.out.println("Nombre: ");
-        nombre=leer.next();
-        System.out.println("Casa a la que pertnece:\n1.Gryffindor\n2.Slytherin\n3.Ravenclaw\n4.Hufflepuff");
-        int op = leer.nextInt();
-        switch (op) {
-            case 1:
-                casa_jugador = "Gryffindor";
-                break;
-            case 2:
-                casa_jugador = "Slytherin";
-                break;
-            case 3:
-                casa_jugador = "Ravenclaw";
-                break;
-            case 4:
-                casa_jugador = "Hufflepuff";
-                break;       
-        }
-        System.out.println("Año que cruza:");
-        año=leer.nextInt();
-        
-        
     }
 }
